85435fbc7c7f936e7ece924520b1624f9f6a484e
samuel-shyaka-dus <samyshyaka@gmail.com>
2022-02-08 13:04:23 +0200

    created a route for the landing page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { 
+    BrowserRouter,
+    Routes,
+    Route 
+} from 'react-router-dom';
 import ReactDOM from 'react-dom';
+import App from './App.js'
 
 ReactDOM.render(
-    <div>
-        <h1>Phantom</h1>
-        <p>powered by CodeBandits</p>
-    </div>,
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+        </Routes>        
+    </BrowserRouter>,
   document.getElementById('root')
 );
