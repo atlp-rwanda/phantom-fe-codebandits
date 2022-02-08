6c31cdc86a3cd01ab0d1f81ad4b9b9daa4498275
samuel-shyaka-dus <samyshyaka@gmail.com>
2022-02-08 13:02:24 +0200

    created the App component

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = ()=> {
+    return(
+        <div>
+            <h1>Phantom</h1>
+            <p>powered by CodeBandits</p>
+        </div>
+    )
+}
+
+export default App
