4c9bf41946df0747a08b4749fe891d3e71880e77
Adeodatus <adeoabdul.a@gmail.com>
2022-02-10 10:37:23 +0200

    coverage added in package.json export {App} modified into export default App space added between Lines

--- tests/App.test.js
+++ tests/App.test.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { shallow } from "enzyme";
-import { App } from "../src/App";
 
+import App from "../src/App";
 
-describe('Test App Entry Point', function (){
-    it('Should have a header tag with Phantom', function () {
-        const wrapper = shallow(<App/>);
-        expect(wrapper.find("h1").text()).toEqual("Phantom");
-    });
+describe("Test App Entry Point", () => {
+  it("Should have a header tag with Phantom", () => {
+    const wrapper = shallow(<App />);
 
-    it('Should have a p tag with powered by CodeBandits', function () {
-        const wrapper = shallow(<App/>);
-        expect(wrapper.find("p").text()).toEqual("powered by CodeBandits");
-    });
+    expect(wrapper.find("h1").text()).toEqual("Phantom");
+  });
+
+  it("Should have a p tag with powered by CodeBandits", () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find("p").text()).toEqual("powered by CodeBandits");
+  });
 });
