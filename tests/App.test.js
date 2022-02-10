f73edb1e76467e2d5ee234aa92bea9a1c3f61207
Adeodatus <adeoabdul.a@gmail.com>
2022-02-10 10:37:23 +0200

    coverage added in package.json export {App} modified into export default App space added between Lines

--- tests/App.test.js
+++ tests/App.test.js
@@ -101,3 +101,19 @@ describe('COunter component', ()=>{
 // });
 
 
+
+import App from "../src/App";
+
+describe("Test App Entry Point", () => {
+  it("Should have a header tag with Phantom", () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find("h1").text()).toEqual("Phantom");
+  });
+
+  it("Should have a p tag with powered by CodeBandits", () => {
+    const wrapper = shallow(<App />);
+
+    expect(wrapper.find("p").text()).toEqual("powered by CodeBandits");
+  });
+});
