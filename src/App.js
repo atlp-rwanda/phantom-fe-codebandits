690239a76852797a5890a4bc131608ed9dfa849c
Justine Musinda Kadhuwa <jaymusinda@live.com>
2022-02-14 18:51:38 +0300

    Revert "#CP-27 Setup Test with Jest & Enzyme"

--- src/App.js
+++ src/App.js
@@ -16,23 +16,14 @@ function App() {
       <h1>Phantom</h1>
       <p>Powered by Codebandits</p>
       <div className="counter">
-        <button
-          id="decrement"
-          type="button"
-          onClick={() => dispatch(decrement())}
-        >
+        <button type="button" onClick={() => dispatch(decrement())}>
           -
         </button>
         <h3 className="counter-value">{value}</h3>
-        <button
-          id="increment"
-          type="button"
-          onClick={() => dispatch(increment())}
-        >
+        <button type="button" onClick={() => dispatch(increment())}>
           +
         </button>
         <button
-          id="reset"
           type="button"
           className="reset"
           onClick={() => dispatch(reset())}
