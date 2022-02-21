4576fb098871dba821bd5a26fec07ae6caa24881
Fabrice Hafashimana <feytoninc@hotmail.com>
2022-02-21 20:51:53 +0200

    ch-(tailwind)-Integrate tailwind
    Successfully add tailwind css
    Replace all prior styles with the tailwind css
    
    Successfully adds all required configurations

--- src/App.js
+++ src/App.js
@@ -12,21 +12,25 @@ function App() {
   const dispatch = useDispatch();
 
   return (
-    <div className="wrapper">
-      <h1>Phantom</h1>
-      <p>Powered by Codebandits</p>
-      <div className="counter">
+    <div className="flex flex-col justify-center text-center items-center h-screen">
+      <h1 className="text-5xl mb-4 text center font-bold font-rale text-primary">
+        Phantom
+      </h1>
+      <p className="pb-8">Powered by Codebandits</p>
+      <div className="pb-8 flex flex-row items-center">
         <button
           id="decrement"
           type="button"
+          className="bg-gray-400 rounded-md px-2 font-bold flex items-center py-0 justify-center"
           onClick={() => dispatch(decrement())}
         >
           -
         </button>
-        <h3 className="counter-value">{value}</h3>
+        <h3 className="mx-2 font-bold">{value}</h3>
         <button
           id="increment"
           type="button"
+          className="bg-gray-400 rounded-md px-2 font-bold flex items-center py-0 justify-center"
           onClick={() => dispatch(increment())}
         >
           +
@@ -34,12 +38,13 @@ function App() {
         <button
           id="reset"
           type="button"
-          className="reset"
+          className="ml-3 bg-gray-400 px-2 rounded-md font-bold hover:text-hover transition-all 3s"
           onClick={() => dispatch(reset())}
         >
           Reset
         </button>
       </div>
+      <q className="font-bold">Added the tailwind css</q>
     </div>
   );
 }
