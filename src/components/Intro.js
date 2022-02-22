fed1e03d14caa6c6e665b8ebe0ccf3d9d59fb1f3
samuel-shyaka-dus <samyshyaka@gmail.com>
2022-02-22 17:30:18 +0200

    update intro

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,22 +4,22 @@ import bus from '../assets/bus.png';
 
 const Intro = () => {
   return (
-    <div className="flex flex-col xl:flex-row">
+    <div className="flex flex-col items-center xl:flex-row">
       <div>
-        <h1 className="flex flex-col text-5xl font-medium mx-28 mt-24">
+        <h1 className="flex flex-col w-max text-3xl xl:text-5xl xl:text-5xl font-raleway font-extrabold mx-14 xl:mx-28 mt-12 xl:mt-32">
           <span>Track bus location,</span>
-          <span>Save your time.</span>
+          <span className="ml-4">Save your time.</span>
         </h1>
-        <p className="flex flex-col mx-40 mt-8">
+        <p className="flex flex-col text-sm xl:text-base mx-20 xl:ml-36 mt-10">
           <span>Check the location of the buses</span>
-          <span>on their respective routes</span>
+          <span className="ml-4">on their respective routes</span>
         </p>
         <Button
           name="Start"
-          styles="bg-blue-600 text-white rounded-3xl mt-10 ml-6 px-12 py-2 ml-48"
+          styles="bg-blue-600 text-white rounded-3xl mt-12 px-12 py-2 ml-28 xl:ml-44"
         />
       </div>
-      <div className="w-96 ml-auto mr-48 mt-28">
+      <div className="w-full px-20 xl:px-0 xl:ml-auto xl:mr-36 xl:px-20 mt-28">
         <img src={bus} alt="Bus" />
       </div>
     </div>
