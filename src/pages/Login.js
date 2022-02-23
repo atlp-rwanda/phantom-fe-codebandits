308a168580f7372ce1e52775bd674a0f960ef0b6
ivad <yvesyvad@gmail.com>
2022-02-23 08:55:21 +0200

    fix styling and add dummy nav & footer

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,26 +2,32 @@ import React from 'react';
 import busMapImg from '../assets/busMap.png';
 const Login = () => {
   return (
-    <div className="font-rale flex  min-h-screen justify-center">
-      <div className="flex w-fit mx-auto m-4">
-        <header>
-          <h1>Phanthom</h1>
-        </header>
-        <div className="w-1/2 m-0">
-          <img src={busMapImg} alt="" className="object-cover h-full w-full" />
+    <div className="font-rale flex flex-col">
+      <header className="w-full h-12 flex  items-center shadow-sm">
+        <h2 className="font-bold text-primary text-center">Phanthom nav bar</h2>
+      </header>
+      <section className="flex w-fit items-center justify-center p-4 m-10 shadow-md">
+        <div className="w-1/2">
+          <img
+            src={busMapImg}
+            alt="Vector image of bus on map"
+            className="object-cover w-fit hidden sm:block"
+          />
         </div>
-        <form className="flex flex-col p-10 self-center shadow-lg">
-          <h1 className="mb-10 text-center">Login</h1>
+        <form className="flex flex-col  p-10 self-center sm:shadow-md">
+          <h2 className="text-3xl mb-10 text-center">Login</h2>
           <input
             type="email"
+            d
             placeholder="Email"
-            className="rounded-md mb-5 p-1 border-2 w-fit"
+            className="rounded-md mb-5 p-1 border-2 w-full"
           />
           <input
             type="password"
             placeholder="Password"
-            className="rounded-md mb-5 p-1 border-2 w-fit"
+            className="rounded-md mb-2 p-1 border-2 w-fit"
           />
+          <h4 className="text-primary text-right">forgot password?</h4>
           <button
             className="bg-primary px-3 py-1 rounded-md text-white w-fit mx-auto hover:bg-hover transition-all "
             type="submit"
@@ -29,7 +35,10 @@ const Login = () => {
             Login
           </button>
         </form>
-      </div>
+      </section>
+      <footer className="w-full h-12 flex  items-center shadow-sm">
+        <h2 className="font-bold text-primary text-center">Phanthom Footer</h2>
+      </footer>
     </div>
   );
 };
