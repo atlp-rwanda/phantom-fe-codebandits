de6ed6b3d33d5776800a9fbd61d4053e5630965f
ivad <yvesyvad@gmail.com>
2022-02-24 09:37:54 +0200

    setup validation

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,5 +1,6 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
+const [formValues, setFormValues] = useState();
+
 
 const Input = ({ name, placeholder, icon, onChange }) => {
   return (
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import busMapImg from '../assets/busMap.png';
 import Input from '../components/Input.js';
 import { Header } from '../components/Dummy.js';
 import { Footer } from '../components/Dummy.js';
 
 const Login = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <div>
       <Header />
       <main className="grid grid-cols-2 items-center w-full justify-center shadow-md">
-        <section>
+        <section className="">
           <img
             src={busMapImg}
             alt="vector image of a bus on map"
             className="w-full object-cover"
           />
         </section>
-        <section className="">
+        <section className="p-10">
           <h2 className="text-3xl mb-10 text-center">Login</h2>
-          <form className="flex flex-col  p-10 self-center sm:shadow-md">
+          <form
+            className="flex flex-col  p-10 self-center shadow-main"
+            onSubmit={handleSubmit}
+          >
             <Input name="email" placeholder="Enter email" />
             <Input name="password" placeholder="Enter password" />
             <h4 className="text-primary text-right">forgot password?</h4>
