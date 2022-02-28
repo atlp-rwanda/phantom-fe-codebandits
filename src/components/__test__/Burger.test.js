3aeed5fb814d1f8fe2962195e6a9ba3e5a965e2d
samuel-shyaka-dus <samyshyaka@gmail.com>
2022-02-28 13:15:03 +0200

    add more tests

--- src/components/__test__/Burger.test.js
+++ src/components/__test__/Burger.test.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { MemoryRouter } from 'react-router-dom';
 import Burger from '../Burger.js';
 
 describe('Burger Component', () => {
-  it('should have three horizontal lines', () => {
-    const wrapper = shallow(
+  it('should have a burger component', () => {
+    const wrapper = mount(
       <MemoryRouter>
         <Burger />
       </MemoryRouter>
     );
     const value = wrapper.children().length;
-    expect(value).toEqual(3);
+    expect(value).toEqual(1);
   });
   it('it should render the Burger component', () => {
     const burger = renderer
--- src/components/__test__/SkeletonScreen.test.js
+++ src/components/__test__/SkeletonScreen.test.js
@@ -3,15 +3,15 @@ import { MemoryRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
 import SkeletonScreen from '../SkeletonScreen.js';
 
+const component = (
+  <MemoryRouter>
+    <SkeletonScreen />
+  </MemoryRouter>
+);
+
 describe('SkeletonScreen', () => {
   it('should render SkeletonScreen', () => {
-    const skelUI = renderer
-      .create(
-        <MemoryRouter>
-          <SkeletonScreen />
-        </MemoryRouter>
-      )
-      .toJSON();
+    const skelUI = renderer.create(component).toJSON();
     expect(skelUI).toMatchSnapshot();
   });
 });
