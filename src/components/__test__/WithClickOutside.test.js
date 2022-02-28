290501b2267048a269a73af931484c839f2f7deb
samuel-shyaka-dus <samyshyaka@gmail.com>
2022-02-28 13:15:03 +0200

    add more tests

--- src/components/__test__/WithClickOutside.test.js
+++ src/components/__test__/WithClickOutside.test.js
@@ -2,11 +2,9 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import WithClickOutside from '../WithClickOutside.js';
 
-const wrapper = <WithClickOutside />;
-
 describe('withClickOutside', () => {
   it('should render withClickOutside', () => {
-    const withClickOutside = renderer.create(wrapper).toJSON();
+    const withClickOutside = renderer.create(<WithClickOutside />).toJSON();
     expect(withClickOutside).toMatchSnapshot();
   });
 });
