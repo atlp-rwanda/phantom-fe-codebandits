528f2ffc1f28b9d643014a317c54c297341e462c
ivad <yvesyvad@gmail.com>
2022-03-01 02:34:41 +0200

    add tests for the sign in page
    only add structure test functionality tests will be added after

--- src/pages/tests/Login.test.js
+++ src/pages/tests/Login.test.js
@@ -1,3 +1,4 @@
+import { shallow } from 'enzyme';
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import renderer from 'react-test-renderer';
@@ -8,6 +9,11 @@ const wrapper = renderer.create(
     <Login />
   </BrowserRouter>
 );
+const mockedUsedNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUsedNavigate
+}));
 
 describe('Login', () => {
   it('should render Login', () => {
@@ -23,5 +29,49 @@ describe('Login', () => {
   it('test the onValid function', () => {
     const onValid = jest.fn(wrapper.getInstance(), 'onValid');
     expect(onValid).toMatchSnapshot();
+  });
+});
+
+const wrapper = shallow(<Login />);
+console.log(wrapper.debug());
+
+describe('Login page tests', () => {
+  it('renders without errors', () => {
+    wrapper;
+  });
+  it('has one child element', () => {
+    expect(wrapper.children().length).toEqual(1);
+  });
+  it('has one form element', () => {
+    expect(wrapper.find('form').length).toEqual(1);
+  });
+  it('has the image of a bus', () => {
+    expect(wrapper.find('img').prop('alt')).toEqual(
+      'vector image of a bus on map'
+    );
+  });
+  it('has a h2 with text login', () => {
+    expect(wrapper.find('h2').text()).toEqual('Login');
+  });
+  it('has one button', () => {
+    expect(wrapper.find('button').length).toEqual(1);
+  });
+  it('has a button with text Login', () => {
+    expect(wrapper.find('button').text()).toEqual('Login');
+  });
+  it('has two input elements', () => {
+    expect(wrapper.find('input').length).toEqual(2);
+  });
+  it('has input for email', () => {
+    expect(wrapper.find('input[name="email"]').length).toBe(1);
+  });
+  it('has input for password', () => {
+    expect(wrapper.find('input[name="password"]').length).toBe(1);
+  });
+  it('has one h4 elemet', () => {
+  expect(wrapper.find('h4').length).toEqual(1);
+  });
+  it('has forgot password heading', () => {
+    expect(wrapper.find('h4').text()).toEqual('Forgot password?');
   });
 });
