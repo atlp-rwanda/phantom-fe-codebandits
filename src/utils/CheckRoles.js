c0c9020c7344e9ef726c493c0d974b2da348eb91
Fabrice Hafashimana <feytoninc@hotmail.com>
2022-04-04 22:08:30 +0200

    feat (languages)- Support multiple language
    - The project is set to use multiple language
    - The selected language is being sent on each request
    
    [Finishes #CP-4]

--- src/utils/CheckRoles.js
+++ src/utils/CheckRoles.js
@@ -16,10 +16,13 @@ function CheckRole({ children, role, type }) {
   } else if (type === 'page' && !role.includes(user.role)) {
     return (
       <div className="flex flex-col items-center justify-center">
-        <h3 className="font-rale font-bold">
-          Don't have the permissions to view the page.
+        <h3 className="font-rale font-bold text-3xl mb-4 mt-5">
+          You don't have the permissions to view the page.
         </h3>
-        <Link to="/login" className="bg-primary rounded-sm font-bold">
+        <Link
+          to="/login"
+          className="bg-primary rounded-sm font-bold py-1 px-3 text-white"
+        >
           Login with a different account
         </Link>
       </div>
